d2b7181fe38f58046e350a9d89c74ec6327b42cd
Sibasish Mohanty <sibasish.mohanty@jupiter.money>
2022-06-11 16:53:32 +0530

    contentlayer setup

---
+++ contentlayer.config.ts
@@ -0,0 +1,57 @@
+import {
+	ComputedFields,
+	defineDocumentType,
+	makeSource,
+} from 'contentlayer/source-files';
+import remarkEmoji from 'remark-emoji';
+import remarkGfm from 'remark-gfm';
+import remarkSlug from 'remark-slug';
+import { getTableOfContents } from './src/utils/mdx-utils';
+// import { rehypeMdxCodeMeta } from './src/utils/rehype-code-meta'
+
+const computedFields: ComputedFields = {
+	slug: {
+		type: 'string',
+		resolve: doc => `/${doc._raw.flattenedPath}`,
+	},
+};
+
+const Doc = defineDocumentType(() => ({
+	name: 'Doc',
+	filePathPattern: 'docs/**/*.mdx',
+	contentType: 'mdx',
+	fields: {
+		title: { type: 'string', required: true },
+		package: { type: 'string' },
+		description: { type: 'string', required: true },
+		image: { type: 'string' },
+		version: { type: 'string' },
+		author: { type: 'string' },
+	},
+	computedFields: {
+		...computedFields,
+		frontMatter: {
+			type: 'json',
+			resolve: doc => ({
+				title: doc.title,
+				package: doc.package,
+				description: doc.description,
+				image: doc.image,
+				version: doc.version,
+				slug: `/${doc._raw.flattenedPath}`,
+				headings: getTableOfContents(doc.body.raw),
+			}),
+		},
+	},
+}));
+
+const contentLayerConfig = makeSource({
+	contentDirPath: 'pages',
+	documentTypes: [Doc],
+	mdx: {
+		// rehypePlugins: [rehypeMdxCodeMeta],
+		remarkPlugins: [remarkSlug, remarkGfm, remarkEmoji],
+	},
+});
+
+export default contentLayerConfig;
